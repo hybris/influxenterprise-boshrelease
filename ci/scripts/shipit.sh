4801c46165ecd8c5769e110821562dff577e02c7
Robert Wolujewicz <robert.wolujewicz@sap.com>
2016-10-05 15:00:01 +0200

    Pipeline modified

--- ci/scripts/shipit.sh
+++ ci/scripts/shipit.sh
@@ -60,7 +60,7 @@ git merge --no-edit ${promotion_branch}
 
 bosh target ${BOSH_TARGET}
 
-bosh -n create release --final --with-tarball --version "$version" --name "influxenterprise"
+bosh -n create release --final --with-tarball --version "$version"
 
 # copy to output dir
 source_dir=$(pwd)
